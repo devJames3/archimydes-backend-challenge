4e6441984f1f83683a6deb1ff14173451c2bda35
James Okolie <zebedee788@gmail.com>
2025-08-10 04:03:30 +0100

    hot fix

--- src/validations/user.js
+++ src/validations/user.js
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 2 characters'),
+  name: z.string().min(3, 'Name must be at least 3 characters'),
   email: z.email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
 });
